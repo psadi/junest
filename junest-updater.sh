60778dc297e93cd4fea3246d5e7c7f092cd5acf2
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-03-16 13:47:29 +0100

    Update junest-updater.sh: install YAY from GIT

--- junest-updater.sh
+++ junest-updater.sh
@@ -10,17 +10,21 @@ HOME="$(dirname "$(readlink -f $0)")"
 git clone https://github.com/fsquillace/junest.git ~/.local/share/junest
 ./.local/share/junest/bin/junest setup -i junest-x86_64.tar.gz
 
-# DOWNLOAD YAY-BIN (JuNest uses its own YAY version, we will replace it with an updated one)
-wget $(wget -q https://api.github.com/repos/Jguer/yay/releases/latest -O - | grep -i x86_64 | grep browser_download_url | grep -i tar.gz | cut -d '"' -f 4 | head -1)
-tar fx yay*tar.gz
-mv ./yay_*/yay ./.junest/usr/bin/yay
-rm -R -f yay_*
+# INSTALL YAY
+./.local/share/junest/bin/junest -- sudo pacman --noconfirm -Rcns yay
+./.local/share/junest/bin/junest -- sudo pacman --noconfirm -S --needed git base-devel
+./.local/share/junest/bin/junest -- git clone https://aur.archlinux.org/yay.git
+./.local/share/junest/bin/junest -- cd yay
+./.local/share/junest/bin/junest -- makepkg -si
 
 # BYPASS SIGNATURE CHECK LEVEL
 #sed -i 's/#SigLevel/SigLevel/g' ./.junest/etc/pacman.conf
 #sed -i 's/Required DatabaseOptional/Never/g' ./.junest/etc/pacman.conf
 
 # UPDATE ARCH LINUX IN JUNEST
+./.local/share/junest/bin/junest -- sudo pacman --noconfirm -Rcns base-devel
 ./.local/share/junest/bin/junest -- sudo pacman -Syy
 ./.local/share/junest/bin/junest -- sudo pacman --noconfirm -Syu
 echo yes | ./.local/share/junest/bin/junest -- sudo pacman -Scc
+
+echo 'SUCCESS!\n'
