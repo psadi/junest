e5abc02b0c3fb8a4371b8d88a15bfe43bcefdcc5
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-06-30 03:12:53 +0200

    Update junest-updater.sh

--- junest-updater.sh
+++ junest-updater.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # DOWNLOAD THE ARCHIVE
-wget https://github.com/ivan-hc/junest/releases/download/continuous/junest-x86_64.tar.gz
+wget $(curl -Ls https://api.github.com/repos/ivan-hc/junest/releases/latest | sed 's/[()",{} ]/\n/g' | grep -oi "https.*tar.gz$" | head -1)
 
 # SET APPDIR AS A TEMPORARY $HOME DIRECTORY, THIS WILL DO ALL WORK INTO THE APPDIR
 HOME="$(dirname "$(readlink -f $0)")"
